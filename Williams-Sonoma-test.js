a03ad1736ef3fa602a0721cc815021c997ca7d07
Nabil Furmoli <nabilfurmoli@gmail.com>
2020-09-20 00:56:08 -0700

    Update Williams-Sonoma-test.js

--- Williams-Sonoma-test.js
+++ Williams-Sonoma-test.js
@@ -11,6 +11,9 @@ const { Builder, By, until } = require('selenium-webdriver');
     driver.get(targetUrl);
 
     driver.wait(until.elementLocated({ name: 'image-2' }), 50000).then((element) => {
+        
+        // Although triggering sleep is not ideal when running this automated test script.
+        // But I have added it here to allow the user see the interaction of chrome driver with the web page.
         driver.sleep(40000);
         console.log(element)
         element.click();
